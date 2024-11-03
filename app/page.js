feaf92817faa4b136ec051a744ee04d6d0bdb3c4
Abhishek Jaiswal <iam001abhi@gmail.com>
2024-11-03 19:31:26 +0530

    fix:merge issue

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
+        Time to get started....
       </h1>
     </main>
   );
